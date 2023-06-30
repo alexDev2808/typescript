df1cc5c9d3173da740564d40bdb28615e51b6a0b
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-30 17:51:26 -0600

    Alias y tipos literales

--- src/03-typing.ts
+++ src/03-typing.ts
@@ -4,3 +4,9 @@ let price : number = 18355;
 
 myProductName = "Macbook Pro";
 const includes = myProductName.includes("Pro Max");
+
+
+let varSinValor: number = price || 10;
+
+console.log(varSinValor)
+
